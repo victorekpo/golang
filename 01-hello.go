de1a48abe33cc83b11637653c7790bdc7ceef107
Vic <victor.ekpo@gmail.com>
2024-10-04 20:24:15 -0500

    updates

---
+++ 01-hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hi there!")
+} 
+
+/*
+* Go by Example: Hello World
+  gobyexample.com
+  
+  Our first program will print the classic "hello world" message. The source code is above.
+
+  To run the program, put the code in hello-world.go and use go run.
+
+  Sometimes you'll want to build the program into binaries. We can do thsi using go build.
+  We can then execute the built binaries directly.
+
+  Now that we can run and build basic Go programs, let's learn more about the language.
+  Next lesson: values.go
+*/
---
+++ 02-values.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  )
+
+func main() {
+    fmt.Println("go" + "lang") // Strings can be added together with +.
+
+    fmt.Println("1+1 =", 1+1)  // Integers and floaots
+    fmt.Println("7.0/3.0 =", 7.0/3.0) 
+
+    fmt.Println(true && false) // booleans
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
+
+/*
+* Values
+Go has various value types including strings, integers, floats, booleans, etc.
+
+Next lesson: variables.go
+
+*/
