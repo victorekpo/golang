a747b876e0ae83ef77ea6961bb5fc3cfed777b69
Vic <victor.ekpo@gmail.com>
2024-11-02 22:42:43 -0500

    fmt Scan and getting user input

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func checkError(err error, msg string) {
+	if err != nil {
+		fmt.Println(msg, err)
+		return
+	}
+}
+
 func main() {
 	// We define variables and Go infers the types when we assign values
 	var conferenceName = "Go Conference"
@@ -18,14 +25,21 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets here to attend the conference\n")
 
-	var userName string
-	var userTickets int
-	// ask the user for their input
-	userName = "Tom"
-	userTickets = 2
-	// Print the user input
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 	// Print out the types of variables
 	fmt.Printf("conferenceTickets is of type %T, remaining Tickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
+	var userName string
+	var userTickets int
+
+	// fmt.Scan is used for input from the user, similar to readline in node.js
+
+	// Get the user input
+	_, userNameErr := fmt.Scan(&userName)
+	checkError(userNameErr, "Error while getting userName:")
+
+	_, userTicketsErr := fmt.Scan(&userTickets)
+	checkError(userTicketsErr, "Error while getting userTickets:")
+
+	// Print the user input
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
