a3f6c0a43e20e20a5ae834fa9c195eaecb7318f8
Vic <victor.ekpo@gmail.com>
2024-11-02 22:19:36 -0500

    learning go

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tickets here to attend the conference")
-
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
+	// Using println
+	//fmt.Println("Welcome to", conferenceName, "booking application")
+	//fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	//fmt.Println("Get your tickets here to attend the conference")
+
+	// Using printf (formatted data)
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets here to attend the conference\n")
+
+	var userName string
+	// ask the user for their name
+	userName = "Tom"
+	fmt.Println(userName)
 }
